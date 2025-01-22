423d3029e97362f5508d321621af8e3966f7f414
Danil <gazizovdanil@gmail.com>
2025-01-22 21:43:57 +0500

    Remove sensitive files from git tracking and add config example

---
+++ config.example.py
@@ -0,0 +1,11 @@
+# Директория для сохранения видео
+OUTPUT_DIR = 'downloads'
+
+# Настройки VK API
+VK_CLIENT_ID = 'YOUR_VK_CLIENT_ID'  # ID вашего приложения VK
+VK_GROUP_ID = 'YOUR_GROUP_ID'       # ID группы ВКонтакте (без минуса)
+VK_API_VERSION = '5.131'            # Версия API VK
+
+# Настройки логирования
+LOG_FILE = 'youtube_vk_downloader.log'
+LOG_FORMAT = '%(asctime)s - %(levelname)s - [%(filename)s:%(lineno)d] - %(message)s' 
